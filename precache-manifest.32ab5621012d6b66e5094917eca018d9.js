bfea071dd47e9be9c824e92517636216a7ff152a
Saurav <royalskraj823@gmail.com>
2021-04-12 10:44:41 +0530

    Updates

---
+++ precache-manifest.32ab5621012d6b66e5094917eca018d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "def1e520760fded8c736102b9004617f",
+    "url": "/covid-19-tracker-react/index.html"
+  },
+  {
+    "revision": "fbaebf52f9a24a7ac7d7",
+    "url": "/covid-19-tracker-react/static/css/main.dda38fab.chunk.css"
+  },
+  {
+    "revision": "9f741c1c188dbff08c28",
+    "url": "/covid-19-tracker-react/static/js/2.c1fc93a0.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker-react/static/js/2.c1fc93a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbaebf52f9a24a7ac7d7",
+    "url": "/covid-19-tracker-react/static/js/main.b1607dcd.chunk.js"
+  },
+  {
+    "revision": "012f52b7fad8012e9afd",
+    "url": "/covid-19-tracker-react/static/js/runtime-main.fe4e11dd.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker-react/static/media/image.d7265326.png"
+  }
+]);
